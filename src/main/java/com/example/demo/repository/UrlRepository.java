382d260a401c81e82708d4700c2a5b1e0209211b
viniciusmecosta <costavinicius315@gmail.com>
2025-01-16 11:34:08 -0300

    annotations in repositorys

--- src/main/java/com/example/demo/repository/UrlRepository.java
+++ src/main/java/com/example/demo/repository/UrlRepository.java
@@ -2,7 +2,9 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.Url;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UrlRepository extends JpaRepository<Url, Long> {
     Url findByUrlOriginal(String urlOriginal);
 }
