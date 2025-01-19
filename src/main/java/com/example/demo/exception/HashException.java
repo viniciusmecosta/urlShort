b4684b9e0867ed99111b6c5a70a3110cbff89e8d
viniciusmecosta <costavinicius315@gmail.com>
2025-01-19 01:30:59 -0300

    fix: Exception handler

---
+++ src/main/java/com/example/demo/exception/HashException.java
@@ -0,0 +1,8 @@
+package com.example.demo.exception;
+
+public class HashException extends RuntimeException {
+    public HashException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/main/java/com/example/demo/exception/NoUrlViewException.java
@@ -0,0 +1,7 @@
+package com.example.demo.exception;
+
+public class NoUrlViewException extends RuntimeException {
+    public NoUrlViewException(String message) {
+        super(message);
+    }
+}
